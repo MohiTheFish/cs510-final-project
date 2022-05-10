8b395ee3ae6abee8a0dfe133d4570c7ec4d1c6ea
Muhammed Imran <imranmuhammed2000@gmail.com>
2022-05-10 18:00:16 -0500

    update requirements.txt, use pytesseract

--- backend/flaskApp/data_json_to_plaintext.py
+++ backend/flaskApp/data_json_to_plaintext.py
@@ -2,7 +2,14 @@ import json
 from bs4 import BeautifulSoup
 from markdown import markdown
 import requests
+import cv2
+import pytesseract
+import os
 
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+tmpFile = 'tmp.img'
 
 # Credit to https://gist.github.com/lorey/eb15a7f3338f959a78cc3661fbc255fe
 def markdown_to_text(markdown_string):
@@ -19,12 +26,23 @@ def markdown_to_text(markdown_string):
     # extract text
     soup = BeautifulSoup(html, "html.parser")
 
+    # Convert any image into text
     for img in soup.find_all('img'):
+        # Retreive image bytes
         imgContent = requests.get(img["src"])
         imgBytes = imgContent.content
-        # Convert to text
 
-        imgText = 'some text'
+        # Convert to text
+        with open(tmpFile, 'wb+') as f:
+            f.write(imgBytes)
+
+        cvImg = cv2.imread(tmpFile)
+        config = ('-l eng --oem 1 --psm 3')
+        imgText = pytesseract.image_to_string(cvImg, config=config).replace('\n', ' ').strip()
+
+
+        # imgText = 'some string'
+        # Add text to HTML representation
         imgTextTag = soup.new_string(imgText)
         img.replace_with(imgTextTag)
     
@@ -49,3 +67,4 @@ if __name__ == "__main__":
             outFile.write('\n')
         
         outFile.close()
+        os.remove(tmpFile)
